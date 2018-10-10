25cc11e7dae924ca3afccbf635fde90689fb3378
Lindsey Jin <l44jin@uwaterloo.ca>
2018-10-10 14:11:20 -0400

    ui tweaking

--- web/passport-plus/src/App.vue
+++ web/passport-plus/src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <div id="container">
-    <h1>Passport+</h1>
+  <div class="container">
+    <div class="row">
+      <h1>Passport</h1>
+      <h1 id="plus">+</h1>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
 
 <style>
-body {
-
-}
+  body {
+    background-image: url("../static/img/notebook.png");
+    font-family: 'Lato', sans-serif;
+    font-size: 14px;
+  }
   h1 {
+    font-family: 'Architects Daughter', cursive;
     font-size: 60px;
-    margin: 30px 0;
+    margin: 30px 15px;
+  }
+  #plus {
+    color: #1d7b35;
   }
 </style>
--- web/passport-plus/src/components/exchange-data-table.vue
+++ web/passport-plus/src/components/exchange-data-table.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="app-container">
-    <table class="table table-dark">
+    <div class="card">
+      <div class="card-header">
+        Featured
+      </div>
+      <div class="card-body">
+        This is some text within a card body.
+      </div>
+    </div>
+    <table class="table table-light table-sm table-striped table-responsive">
       <thead>
       <tr>
         <th v-for="table_header in table_headers">{{table_header}}</th>
+        <th/>
       </tr>
       </thead>
       <tbody>
@@ -12,6 +21,7 @@
         <td>{{item['host']}}</td>
         <td>{{returnString(item['languages'])}}</td>
         <td>{{returnString(item['terms'])}}</td>
+        <td><i class="fa fa-plus" aria-hidden="true"></i></td>
       </tr>
       </tbody>
     </table>
@@ -19,49 +29,56 @@
 </template>
 
 <script>
-import axios from 'axios'
-export default {
-  name: 'ExchangeDataTable',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      table_headers: [
-        'Program',
-        'Host Institution',
-        'Languages',
-        'Terms'
-      ],
-      exchange_data: []
-    }
-  },
-  mounted () {
-    // Todo: move to services
-    let self = this
-    axios.get('http://127.0.0.1:5000/exchanges')
-      .then(function (response) {
-        console.log("successful get request")
-        self.exchange_data = response.data
-    })
-      .catch(function (error){
-        // console log error message
-        console.log(error)
-      })
-  },
-  methods: {
-    returnString (list) {
-      if($.type(list) === "string") return list
-      let result = ""
-      for (var i = 0; i < list.length - 1; i++) {
-        result += list[i] + ", "
+  import axios from 'axios'
+
+  export default {
+    name: 'ExchangeDataTable',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        table_headers: [
+          'Program',
+          'Host Institution',
+          'Languages',
+          'Terms'
+        ],
+        exchange_data: []
       }
-      result += list[list.length - 1]
-      return result
+    },
+    mounted () {
+      // Todo: move to services
+      let self = this
+      axios.get('http://127.0.0.1:5000/exchanges')
+        .then(function (response) {
+          console.log("successful get request")
+          self.exchange_data = response.data
+        })
+        .catch(function (error){
+          // console log error message
+          console.log(error)
+        })
+    },
+    methods: {
+      returnString (list) {
+        if(typeof list === 'string') return list
+        let result = ""
+        if (list === undefined || list.length === 0) return result
+        for (var i = 0; i < list.length - 1; i++) {
+          result += list[i] + ", "
+        }
+        result += list[list.length - 1]
+        return result
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .app-container {
+    margin: 0;
+  }
+  .card {
+    margin-bottom: 20px;
+  }
 </style>
